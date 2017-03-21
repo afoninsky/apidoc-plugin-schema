d818b617748f4d26461375921d21c86e76ab1f01
Anfey <vkfont@yandex.ru>
2017-03-21 14:43:02 +0300

    Add simple 'allOf'

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var path = require('path');
 
 var elementParser = require('./parser/api_schema');
 var schemas = {
-	'jsonschema': require('./schema/jsonschema')
+  'jsonschema': require('./schema/jsonschema')
 };
 
 var app = {};
-                    
+
 module.exports = {
   init: function(_app) {
     app = _app;
@@ -21,16 +21,16 @@ function parserSchemaElements(elements, element, block, filename) {
 
     var values = elementParser.parse(element.content, element.source);
     app.log.debug('apischema.path',values.path);
-		if (schemas[values.schema]) {
+    if (schemas[values.schema]) {
       var relativePath=path.join(path.dirname(filename), values.path);
-			var data = fs.readFileSync(relativePath, 'utf8').toString();
-			var new_elements = schemas[values.schema](relativePath, data, values.element, values.group);
+      var data = fs.readFileSync(relativePath, 'utf8').toString();
+      var new_elements = schemas[values.schema](relativePath, data, values.element, values.group);
 
-			// do not use concat
-			for(var i = 0,l=new_elements.length; i<l;i++) {
-				elements.push(new_elements[i]);
-			}
-		}
+      // do not use concat
+      for(var i = 0,l=new_elements.length; i<l;i++) {
+        elements.push(new_elements[i]);
+      }
+    }
   }
   return elements;
 }
--- schema/jsonschema.js
+++ schema/jsonschema.js
@@ -1,7 +1,7 @@
-
+var lodash = require('lodash');
 
 function exists(keys, key) {
-	return keys.indexOf(key) !== -1;
+  return keys.indexOf(key) !== -1;
 }
 
 function formatType(str) {
@@ -11,18 +11,18 @@ function formatType(str) {
 // TODO change _OR_ to |, requires core fix to allow `Empty parser result.`
 // https://github.com/apidoc/apidoc-core/blob/master/lib/parsers/api_param.js
 function makeType(param) {
-	//console.log('makeType',param);
-	param = Array.isArray(param)? param[0]: param;
-	var strarr = [];
-	if (param.format) {
-	    strarr.push(formatType(param.format));
-	    if ( Array.isArray(param.type) && param.type.indexOf('null') !== -1 ) {
+ // console.log('makeType',param);
+  param = Array.isArray(param)? param[0]: param;
+  var strarr = [];
+  if (param.format) {
+      strarr.push(formatType(param.format));
+      if ( Array.isArray(param.type) && param.type.indexOf('null') !== -1 ) {
             strarr.push('Null');
-	    }
-	    return strarr.join('_OR_');
-	}
-	var str = '';
-	if (Array.isArray(param.type)) {
+      }
+      return strarr.join('_OR_');
+  }
+  var str = '';
+  if (Array.isArray(param.type)) {
         param.type.map(function(type){
             str = type;
             if (str === 'array') {
@@ -31,67 +31,67 @@ function makeType(param) {
             strarr.push(formatType(str));
         });
         return strarr.join('_OR_');
-	} else if (param.type) {
-	    str = param.type
-	    if (str === 'array') {			
-    		str = makeType(param.items)+'[]';
-    	}
-    	return formatType(str);
-	}
-	
-	return 'Unknown';
+  } else if (param.type) {
+      str = param.type
+      if (str === 'array') {
+        str = makeType(param.items)+'[]';
+      }
+      return formatType(str);
+  }
+
+  return 'Unknown';
 }
 
 function makeSize(param) {
-	if (param.type === 'array') { param = param.items; }
-	
-	var keys = Object.keys(param);
-	var str = '';
+  if (param.type === 'array') { param = param.items; }
 
-	if (param.type === 'string' && (exists(keys,'minLength') || exists(keys,'maxLength'))) {
-		
-		if ( exists(keys,'minLength') && exists(keys,'maxLength') && param.minLength === param.maxLength ) {
-			return '{'+param.minLength+'}';
-		}
-		
-		str = '{';
-		if (exists(keys,'minLength')) {
-			str += param.minLength;
-		}
-		str += '..';
-		if (exists(keys,'maxLength')) {
-			str += param.maxLength;
-		}
-		str += '}';
-	} else if ( (param.type === 'integer' || param.type === 'number') && (exists(keys,'minimum') || exists(keys,'maximum')) ) {
-		
-		if ( exists(keys,'minimum') && exists(keys,'maximum') && param.minimum === param.maximum ) {
-			return '{'+param.minimum+'}';
-		}
-		
-		str = '{';
-		if (exists(keys,'minimum')) {
-			str += param.minimum;
-		} else {
-			str += '-∞';
-		}
-		str += ' - ';
-		if (exists(keys,'maximum')) {
-			str += param.maximum;
-		} else {
-			str += '∞';
-		}
-		str += '}';
-	}
+  var keys = Object.keys(param);
+  var str = '';
 
-	return str;
+  if (param.type === 'string' && (exists(keys,'minLength') || exists(keys,'maxLength'))) {
+
+    if ( exists(keys,'minLength') && exists(keys,'maxLength') && param.minLength === param.maxLength ) {
+      return '{'+param.minLength+'}';
+    }
+
+    str = '{';
+    if (exists(keys,'minLength')) {
+      str += param.minLength;
+    }
+    str += '..';
+    if (exists(keys,'maxLength')) {
+      str += param.maxLength;
+    }
+    str += '}';
+  } else if ( (param.type === 'integer' || param.type === 'number') && (exists(keys,'minimum') || exists(keys,'maximum')) ) {
+
+    if ( exists(keys,'minimum') && exists(keys,'maximum') && param.minimum === param.maximum ) {
+      return '{'+param.minimum+'}';
+    }
+
+    str = '{';
+    if (exists(keys,'minimum')) {
+      str += param.minimum;
+    } else {
+      str += '-∞';
+    }
+    str += ' - ';
+    if (exists(keys,'maximum')) {
+      str += param.maximum;
+    } else {
+      str += '∞';
+    }
+    str += '}';
+  }
+
+  return str;
 }
 
 function makeAllowedValues(param) {
-	if (param.type === 'array') { param = param.items; }
-	
+  if (param.type === 'array') { param = param.items; }
+
         // convert null to string, add quotes to strings
-	if ( Array.isArray(param.enum) ) {
+  if ( Array.isArray(param.enum) ) {
 
         param.enum = param.enum.map((item) => {
             if (typeof item === 'string') {
@@ -103,157 +103,155 @@ function makeAllowedValues(param) {
         });
     }
 
-	return (Array.isArray(param.enum)) ? '='+param.enum.join(',') : '';
+  return (Array.isArray(param.enum)) ? '='+param.enum.join(',') : '';
 }
 
 function isRequired(schema, key) {
-	if (schema.type === 'array') { schema = schema.items; }
-		
-	// TODO figure out way to display when anyOf, oneOf
-	return (exists(Object.keys(schema),'required') && (schema.required.indexOf(key) !== -1)) || 
-					(exists(Object.keys(schema.properties), key) && schema.properties[key].required);
+  if (schema.type === 'array') { schema = schema.items; }
+
+  // TODO figure out way to display when anyOf, oneOf
+  return (exists(Object.keys(schema),'required') && (schema.required.indexOf(key) !== -1)) ||
+          (exists(Object.keys(schema.properties), key) && schema.properties[key].required);
 }
 
 // NOTE this is not proper jsonschema, likely in v5 w/ merge
-// var merge = require('lodash/fp/merge');
-/*function mergeAllOf(schema) {
-	// TODO update https://github.com/json-schema/json-schema/issues/116
-	if (exists(Object.keys(schema),'allOf')) {
-		for(var i = schema.allOf.length; i--;) {
-			schema.allOf[i] = mergeAllOf(schema.allOf[i]);
-			
-			var required = schema.required || [];
-			required = required.concat(schema.allOf[i].required || []);
-			schema = merge(schema, schema.allOf[i]);
-			if (required.length) schema.required = required;
-		}
-		delete schema.allOf;
-	}
-	return schema;
-}*/
+function mergeAllOf(schema) {
+  // TODO update https://github.com/json-schema/json-schema/issues/116
+  if (exists(Object.keys(schema),'allOf')) {
+    for(var i = schema.allOf.length; i--;) {
+      schema.allOf[i] = mergeAllOf(schema.allOf[i]);
+
+      var required = schema.required || [];
+      required = required.concat(schema.allOf[i].required || []);
+      lodash.merge(schema, schema.allOf[i])
+      if (required.length) schema.required = required;
+    }
+    delete schema.allOf;
+  }
+  return schema;
+}
 
 
 function traverse(schema, p, group) {
-	var params = {};
-	
-	// Case: apiSuccess returns an array
-	/*if (!p && schema.type === 'array'){
-		params['data'] = '{array} data';
-		p = 'data[]';
-	}*/
-	
-	p = p || '';
+  var params = {};
 
-	
-	var properties = {};
-	//schema = mergeAllOf(schema);
-	if (schema.type === 'object'){
-		properties = schema.properties;
-	} else if (schema.type === 'array' && !schema.items) { // catch errors
-	  throw SyntaxError('ERROR: schema array missing items');
-	} else if (schema.type === 'array' && schema.items.type === 'object') {
-		//schema.items = mergeAllOf(schema.items);
-		properties = schema.items.properties;
-	}
-	
-	//console.log('properties',properties);
-	
-	for(var key in properties) {
-		if (!properties.hasOwnProperty(key)) { continue; }
-		var param = properties[key];
-		//console.log('param',param);
-		if (!param) { continue; }
-		
-		var type = makeType(param);
-		var size = makeSize(param);
-		var allowedValues = makeAllowedValues(param);
-		
-		var description = param.description;
-		if (param.type === 'array') {
-			description += ' '+param.items.description;
-		}
-		
-		// make field
-		var parent = p ? p + '.':'';
-		var field = parent + key;
+  // Case: apiSuccess returns an array
+  /*if (!p && schema.type === 'array'){
+    params['data'] = '{array} data';
+    p = 'data[]';
+  }*/
 
-		if (exists(Object.keys(param),'default')) {
-			if (typeof param.default === 'object') {
-				field += '=\''+JSON.stringify(param.default)+'\'';
-			} else {
-				field += '='+param.default;
-			}
-		}
+  p = p || '';
 
-		if ( !isRequired(schema, key) ) {
-			field = '['+field+']';
-		}
-		
-		if (p) key = p + '.' + key;
-		var g = group ? '('+group+') ' : '';
-		// make group
-		params[key] = g+'{'+type+size+allowedValues+'} '+field+' '+description;
-		//console.log(parent+key, params[parent + key])
-		var subs = {};
-		//var subgroup = p ? p+'.' : ''; // TODO apidoc - groups cannot have `.` in them
-		if (param.type === 'array' && param.items.type === 'object') {
-			subs = traverse(param.items, key, group); // subgroup+
-		} else if (param.type === 'object') {
-			subs = traverse(param, key, group); // subgroup+
-		}
-		for(var subKey in subs) {
-			if (!subs.hasOwnProperty(subKey)) { continue; }
-			params[key+'.'+subKey] = subs[subKey];
-		}
-	}
 
-	return params;
+  var properties = {};
+  schema = mergeAllOf(schema);
+
+  if (schema.type === 'object'){
+    properties = schema.properties;
+  } else if (schema.type === 'array' && !schema.items) { // catch errors
+    throw SyntaxError('ERROR: schema array missing items');
+  } else if (schema.type === 'array' && schema.items.type === 'object') {
+    schema.items = mergeAllOf(schema.items);
+    properties = schema.items.properties;
+  }
+
+  for(var key in properties) {
+    if (!properties.hasOwnProperty(key)) { continue; }
+    var param = properties[key];
+    //console.log('param',param);
+    if (!param) { continue; }
+
+    var type = makeType(param);
+    var size = makeSize(param);
+    var allowedValues = makeAllowedValues(param);
+
+    var description = param.description;
+    if (param.type === 'array') {
+      description += ' '+param.items.description;
+    }
+
+    // make field
+    var parent = p ? p + '.':'';
+    var field = parent + key;
+
+    if (exists(Object.keys(param),'default')) {
+      if (typeof param.default === 'object') {
+        field += '=\''+JSON.stringify(param.default)+'\'';
+      } else {
+        field += '='+param.default;
+      }
+    }
+
+    if ( !isRequired(schema, key) ) {
+      field = '['+field+']';
+    }
+
+    if (p) key = p + '.' + key;
+    var g = group ? '('+group+') ' : '';
+    // make group
+    params[key] = g+'{'+type+size+allowedValues+'} '+field+' '+description;
+    //console.log(parent+key, params[parent + key])
+    var subs = {};
+    //var subgroup = p ? p+'.' : ''; // TODO apidoc - groups cannot have `.` in them
+    if (param.type === 'array' && param.items.type === 'object') {
+      subs = traverse(param.items, key, group); // subgroup+
+    } else if (param.type === 'object') {
+      subs = traverse(param, key, group); // subgroup+
+    }
+    for(var subKey in subs) {
+      if (!subs.hasOwnProperty(subKey)) { continue; }
+      params[key+'.'+subKey] = subs[subKey];
+    }
+  }
+
+  return params;
 }
 
 var $RefParser = require('json-schema-ref-parser');
 var jsf = require('json-schema-faker');
 function build (relativePath, data, element, group) {
-	data = JSON.parse(data);
+  data = JSON.parse(data);
 
-	// run sync - https://github.com/BigstickCarpet/json-schema-ref-parser/issues/14
-	var elements = [], done = false;
-	$RefParser.dereference(relativePath, data, {}, function(err, schema) {
-		if (err) {
-			console.error(err);
-		}
+  // run sync - https://github.com/BigstickCarpet/json-schema-ref-parser/issues/14
+  var elements = [], done = false;
+  $RefParser.dereference(relativePath, data, {}, function(err, schema) {
+    if (err) {
+      console.error(err);
+    }
     else if(element.endsWith('Example'))
     {
-        var faker=jsf(schema); 
-		    var g = group ? ''+group+' ' : ' Example: ';
+        var faker=jsf(schema);
+        var g = group ? ''+group+' ' : ' Example: ';
         var con = ' {json} '+g+'\n'+JSON.stringify(faker,null,4);
         var res = {
-          source: '@'+element+con,   
-				  name: element.toLowerCase(),
-				  sourceName: element,
+          source: '@'+element+con,
+          name: element.toLowerCase(),
+          sourceName: element,
           content: con+'\n'
         };
         elements.push(res);
     }
     else
     {
-		  var lines = traverse(schema, null, group);   
-		  for(var l in lines) {
-			   if (!lines.hasOwnProperty(l)) { continue; }
-			
-			   var res = { 
-				  source: '@'+element+' '+lines[l]+'\n',
-				  name: element.toLowerCase(),
-				  sourceName: element,
-				  content: lines[l]+'\n'
-			   }; 
-			   elements.push(res);
-		  }
+      var lines = traverse(schema, null, group);
+      for(var l in lines) {
+         if (!lines.hasOwnProperty(l)) { continue; }
+
+         var res = {
+          source: '@'+element+' '+lines[l]+'\n',
+          name: element.toLowerCase(),
+          sourceName: element,
+          content: lines[l]+'\n'
+         };
+         elements.push(res);
+      }
     }
-		done = true;
-	});
-	require('deasync').loopWhile(function(){return !done;});
-	//console.log('generated', elements);
-	return elements;
+    done = true;
+  });
+  require('deasync').loopWhile(function(){return !done;});
+  //console.log('generated', elements);
+  return elements;
 }
 
 module.exports = build;
